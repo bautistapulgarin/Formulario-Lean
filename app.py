cccd81747535686b2f7d2527da7b60dd3856193d
bautistapulgarin <bautistapulgarin@gmail.com>
2026-01-19 10:18:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,12 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 
-# Leer credenciales desde Secrets
-creds_dict = json.loads(st.secrets["GSPREAD_CREDS"])
+creds_dict = json.loads(st.secrets["GSPREAD_CREDS"])  # Ahora funciona
 
-# Configurar acceso a Google Sheets
 scope = ["https://spreadsheets.google.com/feeds",
          "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
-# Abrir hoja por ID
 sheet = client.open_by_key("1G8GvBqkTs7WamLRe_h-s9tJxRCCsNtmZEl15WXIggP4").sheet1
-
-# Leer datos
 st.write(sheet.get_all_records())
