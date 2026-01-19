8386916442a35c3e44d812a61e9305d48286548e
bautistapulgarin <bautistapulgarin@gmail.com>
2026-01-19 08:01:45 -0500

    Create app.py
    Initial

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Formulario base", layout="centered")
+
+st.title("Formulario en construcción")
+st.write("Este es el punto de partida del formulario.")
