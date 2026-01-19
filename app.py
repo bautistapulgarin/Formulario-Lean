c01d05c942f3396b272ea265a637fa260abc2c2d
bautistapulgarin <bautistapulgarin@gmail.com>
2026-01-19 10:23:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,18 @@
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
-import json
 
-# Leer credenciales desde secrets
-creds_dict = json.loads(st.secrets["GSPREAD_CREDS"])
+# Ruta del JSON en el repo
+json_file = "credentials.json"
 
-# Definir alcance de Google Sheets
+# Alcances
 scope = ["https://www.googleapis.com/auth/spreadsheets",
          "https://www.googleapis.com/auth/drive"]
 
-# Crear credenciales
-creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
-
-# Conectar con Google Sheets
+# Credenciales
+creds = Credentials.from_service_account_file(json_file, scopes=scope)
 client = gspread.authorize(creds)
 
-# Abrir hoja por ID
+# Abrir hoja
 sheet = client.open_by_key("1G8GvBqkTs7WamLRe_h-s9tJxRCCsNtmZEl15WXIggP4").sheet1
-
-# Leer datos
 st.write(sheet.get_all_records())
